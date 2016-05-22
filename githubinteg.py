dcc4805188afa1ec3ce4eca2933450e94e91fa04
kojiagile <cowboyjpn1919@gmail.com>
2016-05-23 01:13:10 +1000

    Add _roundNumbers function

--- githubinteg.py
+++ githubinteg.py
@@ -2,6 +2,14 @@
 def main(request):
     _roundNumber(4.12345, 2)
     
+
+def _roundNumbers(targetArray, digits):
+    index = 0
+    for num in targetArray:
+        targetArray[index] = _roundNumber(num, digits)
+        index = index + 1
+
+    return targetArray
     
 def _roundNumber(target, digits):
     return round(target, digits)
